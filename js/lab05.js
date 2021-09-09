9e1a1b95a7a2fad7c56649660c3471e5dca8e1e2
Milton Huaripata <mhuaripatac13@unc.edu.pe>
2021-09-09 07:35:15 -0500

    final

--- js/lab05.js
+++ js/lab05.js
@@ -2,35 +2,34 @@
 
     window.onload = function() {
 
-        let AN = document.getElementById("APEnom");
-        let Fec = document.getElementById("FECnac");
-        let AP = document.getElementById("APEpat");
-        let AM = document.getElementById("APEmat");
-        let Noms = document.getElementById("NOM");
-        let LA = document.getElementById("LONape");
-        let E = document.getElementById("EDA");
-        let ML = document.getElementById("MES");
+        let apellidosnombres = document.getElementById("apenom");
+        let fechanacimiento = document.getElementById("fecnac");
+        let apaterno = document.getElementById("apepat");
+        let amaterno = document.getElementById("apemat");
+        let nombres = document.getElementById("nom");
+        let longapellidos = document.getElementById("lonape");
+        let edad = document.getElementById("edad");
+        let mess = document.getElementById("mes");
         let butextraer = document.getElementById("extraer");
         let button = document.getElementById("colorear");
 
         butextraer.addEventListener("click", function() {
 
-            const APnom = document.getElementById('IAPEnom');
+            const APnom = document.getElementById('iapenom');
             let APnombres = APnom.value;
             let NMat = APnombres.split(/\s+/);
-            const PAT = document.getElementById('IAPEpat');
+            const PAT = document.getElementById('iapepat');
             PAT.value = NMat[0];
-            const MAT = document.getElementById('IAPEmat');
+            const MAT = document.getElementById('iapemat');
             MAT.value = NMat[1];
-            const NomCompletos = document.getElementById('INOM');
+            const NomCompletos = document.getElementById('inom');
 
             for (let i = 2; i < NMat.length; i++) {
                 NomCompletos.value += NMat[i] + " ";
             }
+            document.getElementById("ilonape").value = ((PAT.value + MAT.value).length);
 
-            document.getElementById("ILONape").value = ((PAT.value + MAT.value).length + " letras ");
-
-            let fecha = document.getElementById("IFECnac").value;
+            let fecha = document.getElementById("ifecnac").value;
             let a = new Date();
             let cump = new Date(fecha);
             let edad = a.getFullYear() - cump.getUTCFullYear();
@@ -45,12 +44,12 @@
                 }
             }
 
-            document.getElementById("IEDA").value = (+edad + " años");
+            document.getElementById("iedad").value = (+edad);
             let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
-                "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+                "Julio", "Agosto", "Setiembre", "Octubre", "Noviembre", "Diciembre"
             ];
             let dia = new Date(cump);
-            document.getElementById("IMES").value = (meses[dia.getMonth()]);
+            document.getElementById("imes").value = (meses[dia.getMonth()]);
         });
 
         button.addEventListener("click", pintarCajas);
@@ -59,25 +58,27 @@
 
         function pintarCajas() {
             if (cont % 2 == 0) {
-                AN.style.backgroundColor = "#a51616";
-                Fec.style.backgroundColor = "#a51616";
-                AP.style.backgroundColor = "#a51616";
-                AM.style.backgroundColor = "#a51616";
-                Noms.style.backgroundColor = "#a51616";
-                LA.style.backgroundColor = "#1468b8";
-                E.style.backgroundColor = "#1468b8";
-                ML.style.backgroundColor = "#1468b8";
+                apellidosnombres.style.backgroundColor = "rgb(100, 14, 14)";
+                fechanacimiento.style.backgroundColor = "rgb(100, 14, 14)";
+                apaterno.style.backgroundColor = "rgb(100, 14, 14)";
+                amaterno.style.backgroundColor = "rgb(100, 14, 14)";
+                nombres.style.backgroundColor = "rgb(100, 14, 14)";
+
+                longapellidos.style.backgroundColor = "rgb(6, 62, 114)";
+                edad.style.backgroundColor = "rgb(6, 62, 114)";
+                mess.style.backgroundColor = "rgb(6, 62, 114)";
             }
 
             if (cont % 2 != 0) {
-                AN.style.backgroundColor = "#1468b8";
-                Fec.style.backgroundColor = "#1468b8";
-                AP.style.backgroundColor = "#1468b8";
-                AM.style.backgroundColor = "#1468b8";
-                Noms.style.backgroundColor = "#1468b8";
-                LA.style.backgroundColor = "#a51616";
-                E.style.backgroundColor = "#a51616";
-                ML.style.backgroundColor = "#a51616";
+                apellidosnombres.style.backgroundColor = "rgb(6, 62, 114)";
+                fechanacimiento.style.backgroundColor = "rgb(6, 62, 114)";
+                apaterno.style.backgroundColor = "rgb(6, 62, 114)";
+                amaterno.style.backgroundColor = "rgb(6, 62, 114)";
+                nombres.style.backgroundColor = "rgb(6, 62, 114)";
+
+                longapellidos.style.backgroundColor = "rgb(100, 14, 14)";
+                edad.style.backgroundColor = "rgb(100, 14, 14)";
+                mess.style.backgroundColor = "rgb(100, 14, 14)";
             }
             cont = cont + 1;
         }
